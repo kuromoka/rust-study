f8bc15ce3d80321cc048030f1ae8256fd09f60b1
kuromoka <kuromokapb@gmail.com>
2018-08-31 23:22:21 +0900

    Add target folder in gitignore

--- hello_world/target/debug/deps/hello_world-0a62123b3ce75fba.d
+++
@@ -1,5 +0,0 @@
-/Users/yu/Dropbox/Projects/rust-study/hello_world/target/debug/deps/hello_world-0a62123b3ce75fba: src/main.rs
-
-/Users/yu/Dropbox/Projects/rust-study/hello_world/target/debug/deps/hello_world-0a62123b3ce75fba.d: src/main.rs
-
-src/main.rs:
--- hello_world/target/debug/hello_world.d
+++
@@ -1,1 +0,0 @@
-/Users/yu/Dropbox/Projects/rust-study/hello_world/target/debug/hello_world.dSYM: /Users/yu/Dropbox/Projects/rust-study/hello_world/src/main.rs
